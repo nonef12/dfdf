f1fdb0e6c9cee3d63c7272f8b6e88558917b7a59
nonef12 <62209010017@tatc.ac.th>
2022-10-31 16:42:18 +0700

    first commit

---
+++ src/modules/axios.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const defaultAxios = axios.create({
+  baseURL: '/api',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export const install = ({ app, store }) => {
+  app.config.globalProperties.$axios = defaultAxios
+  app.config.globalProperties.$http = defaultAxios
+}
+
+
